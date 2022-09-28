aab436d9def55de1f9306f488a20822cc3859933
chavda-rajan <www.chavdarajan@gmail.com>
2022-09-28 17:38:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const ITEMS = [
+  { text: 'first', text_id: 1 },
+  { text: 'second', text_id: 2 },
+  { text: 'third', text_id: 3 },
+  { text: 'fourth', text_id: 4 },
+  { text: 'fifth', text_id: 5 },
+  { text: 'sixth', text_id: 6 },
+  { text: 'seventh', text_id: 7 },
+  { text: 'eight', text_id: 8 },
+];
+
 function App() {
+  const [list, setList] = useState([]);
+  const [drag, setDrag] = useState({});
+  const [dragIndex, setDragIndex] = useState(null);
+
+  const onDragStart = (data) => () => {
+    setDrag(data)
+  }
+
+  const onDragOver = (data) => {
+    data.preventDefault();
+  }
+
+  const onDrop = (data) => {
+    if (!drag.text_id) return;
+    const innerDrop = parseInt(data.target.id) + 1;
+    let list_clone = list;
+    if (innerDrop) {
+      const beforeInnerDropVal = list_clone.slice(0, innerDrop);
+      const afterInnerDropVal = list_clone.slice(innerDrop, list.length);
+      setList([...beforeInnerDropVal, drag, ...afterInnerDropVal]);
+    } else {
+      list_clone.push(drag);
+      setList(list_clone);
+    }
+    setDrag({});
+    setDragIndex(null);
+  }
+
+  const onInnerDragStart = (data) => () => {
+    setDrag(data);
+    const index = list.indexOf(data);
+    setDragIndex(index);
+  };
+
+  const onInnerDragLeave = () => {
+    const list_clone = list;
+    list_clone.splice(dragIndex, 1);
+    setList(list_clone);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className="lists">
+        <div className="list" >
+          {ITEMS.map((elem) => (
+            <div className="list-item" draggable onDragStart={onDragStart(elem)} key={elem.text_id}>{elem.text}</div>
+          ))}
+        </div>
+        <div
+          className="list list2"
+          onDragOver={onDragOver}
+          onDrop={onDrop}
         >
-          Learn React
-        </a>
-      </header>
+          {list.map((elem, index) => (
+            <div
+              className="list-item list-item2"
+              id={index}
+              draggable
+              onDragStart={onInnerDragStart(elem)}
+              // onDragEnd={onInnerDragEnd}
+              onDragLeave={onInnerDragLeave}
+              onDrop={onDrop}
+              key={index}
+            >
+              {elem.text}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
